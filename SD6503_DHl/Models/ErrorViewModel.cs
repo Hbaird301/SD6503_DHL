983b15aa1b52962b5489bb02e16be8ae9bfd2fac
Hamish Baird <hbaird301@gmail.com>
2021-09-21 11:53:12 +1200

    Add project files.

---
+++ SD6503_DHl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SD6503_DHl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
